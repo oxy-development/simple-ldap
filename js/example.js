73812d286a89d38807f40cf43d691f97465c92da
Yuriy Gintsyak <gintsyak@gmail.com>
2016-03-17 00:47:47 +0600

    Made some changes... now it does not work, will contnue tomorrow

--- js/example.js
+++ js/example.js
@@ -3,7 +3,7 @@ function present(name) { return a("symbolicname").eq(name); }
 function notpresent(name) {return a("symbolicname").neq(name); }
 
 
-present("hive.replication.central.*")
+var result = present("hive.replication.central.*")
   .or(present("hive.commons.com.typesafe.scalalogging"))
   .or(present("hive.commons.db"))
   .or(present("hive.commons.configuration"))
